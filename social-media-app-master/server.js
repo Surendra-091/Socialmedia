15e81d517127a97dd912fe3dcdbe4c8f341e2357
Surendra-091 <surendra.22bce20386@vitapstudent.ac.in>
2025-11-04 18:28:41 +0530

    Update server.js

--- social-media-app-master/server.js
+++ social-media-app-master/server.js
@@ -17,8 +17,7 @@ dotenv.config();
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: ["http://localhost:3000", "https://socialmedia-back-qmub.onrender.com
-"],
+    origin: ["http://localhost:3000", "https://socialmedia-back-qmub.onrender.com"],
   },
 });
 
