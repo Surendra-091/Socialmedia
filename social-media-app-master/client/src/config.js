288a6a8e0810868560785e5079212fffbbcaaa51
Surendra-091 <surendra.22bce20386@vitapstudent.ac.in>
2025-11-04 18:56:22 +0530

    Refactor BASE_URL assignment for environment handling

--- social-media-app-master/client/src/config.js
+++ social-media-app-master/client/src/config.js
@@ -1,6 +1,12 @@
-let BASE_URL = "https://socialmedia-back-qmub.onrender.com/";
+let BASE_URL;
+
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  // Local development (frontend connects to localhost backend)
   BASE_URL = "http://localhost:4000/";
+} else {
+  // Production (frontend connects to Render backend)
+  BASE_URL = "https://socialmedia-back-qmub.onrender.com/";
 }
 
 export { BASE_URL };
+
