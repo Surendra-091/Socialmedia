e717a447d94acf916dd6cc4497b91a879e8a4682
Surendra-091 <surendra.22bce20386@vitapstudent.ac.in>
2025-11-04 18:16:20 +0530

    Update CORS origin for Socket.io server

--- social-media-app-master/server.js
+++ social-media-app-master/server.js
@@ -17,7 +17,8 @@ dotenv.config();
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: ["http://localhost:3000", "https://post-it-heroku.herokuapp.com"],
+    origin: ["http://localhost:3000", "https://socialmedia-back-qmub.onrender.com
+"],
   },
 });
 
